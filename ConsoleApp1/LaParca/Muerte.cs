09351fe2c8a375752cf8166c599ec584ecfaf27c
danielmamaninina17-ux <danielmamaninina17@gmail.com>
2026-09-15 10:11:10 -0400

    COMICC
    HOLAAAAAAAA

---
+++ ConsoleApp1/LaParca/Muerte.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.LaParca
+{
+    public class Muerte
+    {
+        public static void Main(string[] args)
+        {
+
+            int a;
+            int b;
+
+            Console.WriteLine("Dame el valor de A: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Dame el valor de B: ");
+            b = int.Parse(Console.ReadLine());
+
+            int suma = a + b;
+
+           Console.WriteLine("La suma es: " + suma );
+
+
+         }
+
+    }
+}
